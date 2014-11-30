8908c09da6f3d7eeacc87fc199ac940e77ee7877
GuiLLeRm0 <gdavid.ptorrez@gmail.com>
2014-11-30 08:14:36 -0400

    Agregando Funcionalidad

--- js/searchTwitter.jquery.js
+++ js/searchTwitter.jquery.js
@@ -1,16 +1,42 @@
+ // Autor: GUillermo David Paredes Torrez
+ // Compani: Redefined Ind
+ // Business: TST
+if( typeof Object.create !== 'function'){
+	Object.create = function (obj) {
+		function F () {};
+		F.prototype = obj;
+		return new F();
+	}
+}
  ;(function($,window,document,undefined){
  	var Twitter = {
+ 		init: function(options, elem){
+ 			var self = this;
+ 			self.elem = elem;
+ 			self.$elem = $(elem);
 
+ 			self.url = 'http://search.twitter.com/search.json';
+
+ 			if( typeof options === 'string') {
+ 				self.search = options
+ 			}else {
+ 				self.search = options.search;
+ 				self.options = $.extend({}, $.fn.queryTwitter.options,options);
+ 				console.log(self.options);
+ 			}
+ 		} 
  	}
- 	$.fn.queryTwitter = function(){
- 		console.log(this.attr('class'));
- 		return $(this).each(function(){
 
+ 	$.fn.queryTwitter = function(options){
+ 		return this.each(function(){
+ 			console.log(Twitter);
+ 			var twitter = Object.create(Twitter);
+			twitter.init(options,this);
  		});
  	};
 
  	$.fn.queryTwitter.options = {
-
+ 		search : '@tutspremium'
  	}
  })($ ,window,document);
 
