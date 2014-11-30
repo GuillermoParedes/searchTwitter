8db396188ff38e410187b3b93d20858a894215ae
GuiLLeRm0 <gdavid.ptorrez@gmail.com>
2014-11-30 00:18:14 -0400

    Plugin JS de Twitter

---
+++ js/searchTwitter.jquery.js
@@ -0,0 +1,17 @@
+ ;(function($,window,document,undefined){
+ 	var Twitter = {
+
+ 	}
+ 	$.fn.queryTwitter = function(){
+ 		console.log(this.attr('class'));
+ 		return $(this).each(function(){
+
+ 		});
+ 	};
+
+ 	$.fn.queryTwitter.options = {
+
+ 	}
+ })($ ,window,document);
+
+ 
